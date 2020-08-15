c8429685d1a63db78e60e6fe46b51a54ebe6eda3
Corey <coreyitaliano@gmail.com>
2020-08-15 02:43:34 -0400

    Added support for multiple twitter channels based off the main account. You can now add keyword configuration for different twitter handles that are to monitor 'The Worst Fans' twitters accounts. If a keyword for a specific channel is present, this will retweet the tweet for that twitter account. For example, if JT Kirkman tweets a tweet about the UFC and uses the word UFC (be it in a hashtag or not), and ufc is a keyword specified for the handle TheWorstFansMMA, then this script will retweet that tweet under TheWorstFansMMA account. Also separated out configuration files for easy expansion to other new sub twitter accounts.

--- reTweeter.php
+++ reTweeter.php
@@ -1,23 +1,12 @@
 <?php
-
-/*
-
-WFSly,1198309936872263683
-JTKirkmanWF,1198263353715691522
-WilsonWildingWF,1197626664110432259
-
-*/
-
 	echo "Checking for new tweets from The Worst Fans...\n";
 
 	require "vendor/autoload.php";
 
 	use Abraham\TwitterOAuth\TwitterOAuth;
-
-	$CONSUMER_KEY = "";
-	$CONSUMER_SECRET = "";
-	$access_token = "";
-	$access_token_secret = "";
+    
+    $PATH_TO_TWITTER_API_CONFIG_FILE = 'F:\Projects\reTweeter\reTweeter.cfg';
+    $PATH_TO_CONFIG_FILE = 'F:\Projects\reTweeter\tweetChannels.cfg';
 	$DATABASE_FILE = "F:\\Projects\\reTweeter\\db.csv";
 	
 	/**
@@ -40,13 +29,60 @@ WilsonWildingWF,1197626664110432259
 			{
 				break;
 			}
-			echo "name : [$latestTweet[0]] \n";
-			echo "id : [$latestTweet[1]] \n";
+			#echo "name : [$latestTweet[0]] \n";
+			#echo "id : [$latestTweet[1]] \n";
 			$lastTweetsMap[$latestTweet[0]] = $latestTweet[1];
 		}
 		echo "getLastTweetsMap lastTweetsMap : [" . print_r($lastTweetsMap, true) . "]\n";
 		return $lastTweetsMap;
 	}
+    
+    /*
+        reads in tweetChannels.cfg which contains keywords assigned to each channel for The Worst Fans    
+    */
+    function getTwitterChannelKeywords()
+    {
+        global $PATH_TO_CONFIG_FILE;
+        $dataString = file_get_contents($PATH_TO_CONFIG_FILE);
+        #echo $dataString;
+        $twitterChannelData = explode("\n",$dataString);
+        $twitterChannelInfoMap = array();
+        foreach($twitterChannelData as $twitterChannelLine)
+        {
+            $twitterChannelLineInfo = explode("=", $twitterChannelLine);
+            $twitterChannelInfoMap[$twitterChannelLineInfo[0]] = explode(",", $twitterChannelLineInfo[1]);
+        }
+        print_r($twitterChannelInfoMap);
+        return $twitterChannelInfoMap;
+    }
+    
+    /*
+        reads in reTweeter.cfg which contains twitter api info for the different accounts
+    */
+    function getTwitterConfig()
+    {
+        global $PATH_TO_TWITTER_API_CONFIG_FILE;
+        $dataString = file_get_contents($PATH_TO_TWITTER_API_CONFIG_FILE);
+        #echo "getTwitterConfig : [" . $dataString . "]";
+        $twitterConfigData = explode("\n",$dataString);
+        $twitterConfigInfoMap = array();
+        foreach($twitterConfigData as $twitterConfigDataLine)
+        {
+            #echo "twitterConfigDataLine [" . print_r($twitterConfigDataLine) . "]cc\n";
+            $twitterConfigDataLineInfo = explode("=", $twitterConfigDataLine);
+            #echo "dd twitterConfigDataLineInfo [" . print_r($twitterConfigDataLineInfo) . "]\n";
+            $accountInfo = explode(".", $twitterConfigDataLineInfo[0]);
+            #echo "name  [" . $accountInfo[0] . "] type [" . $accountInfo[1] . "] val  [" . $twitterConfigDataLineInfo[1] . "]\n";
+            #echo "accountInfo [" . print_r($accountInfo) . "]\n";
+            if($accountInfo[0] != '')
+            {
+                $twitterConfigInfoMap[$accountInfo[0]][$accountInfo[1]] = $text = preg_replace("/\r|\n/", "", $twitterConfigDataLineInfo[1]);
+            }
+        }
+        #echo "twitterConfigInfoMap [" . print_r($twitterConfigInfoMap) . "]\n";
+        #echo "twitterConfigInfoMap [" . $twitterConfigInfoMap . "]\n";
+        return $twitterConfigInfoMap;
+    }
 	
 	/*
 	* Stored last tweet db info
@@ -54,11 +90,11 @@ WilsonWildingWF,1197626664110432259
 	function setLastTweetsMap($lastTweetsMap)
 	{
 		global $DATABASE_FILE;
-		echo "setLastTweetsMap lastTweetsMap : [" . print_r($lastTweetsMap, true) . "]\n";
+		#echo "setLastTweetsMap lastTweetsMap : [" . print_r($lastTweetsMap, true) . "]\n";
 		$databaseData = '';
 		foreach($lastTweetsMap as $twitterHandle => $tweetId)
 		{
-			echo "setLastTweetsMap twitterHandle [$twitterHandle] tweetId [$tweetId]\n";
+			#echo "setLastTweetsMap twitterHandle [$twitterHandle] tweetId [$tweetId]\n";
 			$databaseData .= "$twitterHandle,$tweetId\n";
 		}
 		if($databaseData == '')
@@ -84,25 +120,65 @@ WilsonWildingWF,1197626664110432259
 	*/
 	function createRetweetAndLike($connection, $tweetId)
 	{
-		echo "Retweeting [$tweetId]\n";
-		$connection->post("statuses/retweet", ["id" => $tweetId]);
+		#echo "connection [" . print_r($connection, true) . "]\n";
+        echo "Retweeting [$tweetId]\n";
+		$result = $connection->post("statuses/retweet", ["id" => $tweetId]);
+        #echo "result [" . print_r($result, true) . "]\n";
 		$connection->post("favorites/create", ["id" => $tweetId]);
 	}
-	
+    
+    /*
+	* Builds an API connection
+	*/
+	function buildConnection($twitterConfig)
+	{
+        //connect
+        #echo "buildConnection : [" . print_r($twitterConfig, true). "] key : [" . $twitterConfig['consumer_key'] . "]\n";
+        $connection = new TwitterOAuth($twitterConfig['consumer_key'], $twitterConfig['consumer_secret'], $twitterConfig['access_token'], $twitterConfig['access_token_secret']);
+        $content = $connection->get("account/verify_credentials");
+		return $connection;
+	}
+    
+    /*
+	* Retweets to a worst fans channel if conditions are met
+	*/
+	function retweetToChannelIfApplicable($twitterChannelKeywordInfo, $twitterConfig, $newTweet)
+	{
+        foreach ($twitterChannelKeywordInfo as $twitterChannel=>$twitterChannelKeywords)
+        {
+            echo "twitterChannel : [" . $twitterChannel . "]\n";
+            foreach ($twitterChannelKeywords as $keyword)
+            {
+                echo "rt text [" . $newTweet->text . "] keyword : [" . $keyword . "] \n";
+                if(stripos($newTweet->text, $keyword) !== false)
+                {
+                    echo "Word [" . $keyword . "] Found! RETWEETING!\n";
+                    //retweet
+                    createRetweetAndLike(buildConnection($twitterConfig[$twitterChannel]), $newTweet->id);
+                    break;
+                }
+                else
+                {
+                    echo "Word [" . $keyword . "] Not Found!\n";
+                }
+            }
+        }
+	}
 	#MAIN
-	//connect
-	$connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $access_token, $access_token_secret);
-	$content = $connection->get("account/verify_credentials");
 	$theWorstFansTwitterHandles = array('JTKirkmanWF', 'WilsonWildingWF', 'WFSly');
 	$tweetsToRetweet = array();
 	//get the ids of the last tweets for our worst fans
 	$lastTweetsMap = getLastTweetsMap();
+    $twitterChannelKeywordInfo = getTwitterChannelKeywords();
+    $twitterConfig = getTwitterConfig();
+    $connection = buildConnection($twitterConfig['main']);
 	//loop through each fan
 	foreach($theWorstFansTwitterHandles as $twitterHandle)
 	{
 		$latestTweetId = $lastTweetsMap[$twitterHandle];
 		//get their tweets since the last time we checked
 		$newTweets = getTweets($connection, $twitterHandle, $latestTweetId);
+        #echo "newTweets : [" . print_r($newTweets, true) . "]";
 		//go through what we founds
 		foreach ($newTweets as $newTweet)
 		{
@@ -112,13 +188,14 @@ WilsonWildingWF,1197626664110432259
 			}
 			else if(property_exists($newTweet, "retweeted_status"))
 			{
-				//This is a retweet, ignore
+				//This is a retweet, check to see if we should retweet it in any specific channels
+                retweetToChannelIfApplicable($twitterChannelKeywordInfo, $twitterConfig, $newTweet);
 			}
 			else
 			{
 				//This is a tweet!
-				echo "last recorded tweet id [" . $lastTweetsMap[$twitterHandle]. "] current tweet id : [" . $newTweet->id . "]\n";
-				array_push($tweetsToRetweet, $newTweet->id);
+				#echo "last recorded tweet id [" . $lastTweetsMap[$twitterHandle]. "] current tweet id : [" . $newTweet->id . "]\n";
+				array_push($tweetsToRetweet, $newTweet);
 				if($lastTweetsMap[$twitterHandle] < $newTweet->id)
 				{
 					$lastTweetsMap[$twitterHandle] = $newTweet->id;
@@ -129,11 +206,13 @@ WilsonWildingWF,1197626664110432259
 	//sort the tweets so we retweet them in order
 	sort($tweetsToRetweet);
 	//retweet all the tweets we found
-	foreach($tweetsToRetweet as $tweetId)
+	foreach($tweetsToRetweet as $newTweet)
 	{
-		createRetweetAndLike($connection, $tweetId);
+		createRetweetAndLike($connection, $newTweet->id);
+        //check to see if any channels should rt this
+        retweetToChannelIfApplicable($twitterChannelKeywordInfo, $twitterConfig, $newTweet);
 	}
 	//store the database for next time
 	setLastTweetsMap($lastTweetsMap);
 	echo "Fin.";
-?>  
+?>
